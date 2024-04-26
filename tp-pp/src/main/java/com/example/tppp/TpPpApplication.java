1308d18ebf727cfc8e7609313620133689d2be1a
InesBenyouk <156437005+InesBenyouk@users.noreply.github.com>
2024-04-26 20:50:34 +0100

    Add files via upload

---
+++ tp-pp/src/main/java/com/example/tppp/TpPpApplication.java
@@ -0,0 +1,13 @@
+package com.example.tppp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpPpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpPpApplication.class, args);
+    }
+
+}
---
+++ tp-pp/src/test/java/com/example/tppp/TpPpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tppp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
